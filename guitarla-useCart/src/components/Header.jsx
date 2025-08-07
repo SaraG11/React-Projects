d07c2a720b0e371f2fd7cba1e4e2c6c1a6ed5ced
SG-WORK <gpsara1114@gmail.com>
2025-08-07 13:27:01 -0600

    Creacion de custom hook de header y finalizado

--- guitarla-useCart/src/components/Header.jsx
+++ guitarla-useCart/src/components/Header.jsx
@@ -1,10 +1,6 @@
-import { useMemo } from 'react'
 
-export default function Header({cart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart}) {
+export default function Header({cart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal}) {
 
-    // State Derivado
-    const isEmpty = useMemo( () => cart.length === 0, [cart])
-    const cartTotal = useMemo( () => cart.reduce( (total, item ) => total + (item.quantity * item.price), 0), [cart] )
 
     return (
         <header className="py-5 header">
--- guitarla-useCart/src/hooks/useCart.js
+++ guitarla-useCart/src/hooks/useCart.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { db } from "../data/db";
 
 export const useCart = () => {
@@ -65,6 +65,10 @@ export const useCart = () => {
         setCart([])
       }
 
+      // State Derivado
+    const isEmpty = useMemo( () => cart.length === 0, [cart])
+    const cartTotal = useMemo( () => cart.reduce( (total, item ) => total + (item.quantity * item.price), 0), [cart] )
+
     return {
         data,
         cart,
@@ -72,6 +76,8 @@ export const useCart = () => {
         removeFromCart,
         decreaseQuantity,
         increaseQuantity,
-        clearCart
+        clearCart,
+        isEmpty,
+        cartTotal
     }
 }
